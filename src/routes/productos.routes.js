2753f5aae340e2ffe4fff75186c6a116064ed101
Hidalgo2002 <jehiel.hidalg@gmail.com>
2022-04-29 10:36:24 -0300

    agregamos el controlodar de obtener producto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,13 @@
 // la mision de este archivo es mantener las rutas
 import {Router} from "express"
 import productoCtrl from "../controllers/productos.controllers"
+// onstancio al router
 const router = Router ()
 
 //crear la ruta
 router.route("/products").get(productoCtrl.listarProductos).post(productoCtrl.crearProducto)
+
+// el ":" significa q lo q le sigue es un parametro
+router.route("/products/:id").get(productoCtrl.obtenerProducto)
 
 export default router;
